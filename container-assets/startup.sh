a30c55cb9bca9a3f066d1710e45d676b50f73ac3
Gitesh Koli <gkoli@google.com>
2021-10-26 21:55:19 -0400

    Created a docker compose demo file to use the default generated image

--- container-assets/startup.sh
+++ container-assets/startup.sh
@@ -7,20 +7,18 @@ composer require drupal/restui -o --working-dir=/app/code --no-interaction
 FILE="/app/code/web/sites/default/private/salt.txt"
 
 if [ ! -f "$FILE" ]; then
+  sleep 20; #Sleep for 20 seconds to give the mariadb docker container time to startup
   if [ ! -z "$AUTO_INSTALL_PORTAL" ]; then
-    sleep 20; #Sleep for 20 seconds to give the mariadb docker container time to startup
     $DRUSH si apigee_devportal_kickstart --site-name="Apigee Developer Portal" \
       --account-name="$ADMIN_USER" --account-mail="$ADMIN_EMAIL" \
       --account-pass="$ADMIN_PASS" --site-mail="noreply@apigee.com" \
       --no-interaction
-
-    /set-permissions.sh --drupal_path=/app/code/web --drupal_user=www-data --httpd_group=www-data
-
     $DRUSH en rest restui basic_auth
     $DRUSH config:set key.key.apigee_edge_connection_default key_provider apigee_edge_environment_variables --no-interaction
     $DRUSH cim --partial --source=/app/default-config
     $DRUSH apigee-edge:sync --no-interaction
   fi
+  /set-permissions.sh --drupal_path=/app/code/web --drupal_user=www-data --httpd_group=www-data
 fi
 
 $DRUSH updb -y || true
--- run.sh
+++ run.sh
@@ -14,31 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-docker volume rm public-files || true
-docker volume rm private-files || true
-
-docker volume create public-files
-docker volume create private-files
-
-docker rm -f apigee-devportal-db || true
-
-docker run --name apigee-devportal-db -p 53306:3306 -d \
-    -e MYSQL_DATABASE=apigee_devportal \
-    -e MYSQL_USER=dbuser \
-    -e MYSQL_PASSWORD=passw0rd \
-    -e MYSQL_ROOT_PASSWORD=rootpasswd \
-	mariadb:latest
-
-docker rm -f apigee-devportal || true
-
-docker run --name apigee-devportal -p 8080:80 --env-file=./apigee.env \
-    -e DRUPAL_DATABASE_NAME=apigee_devportal \
-    -e DRUPAL_DATABASE_USER=dbuser \
-    -e DRUPAL_DATABASE_PASSWORD=passw0rd \
-    -e DRUPAL_DATABASE_HOST=host.docker.internal \
-    -e DRUPAL_DATABASE_PORT=53306 \
-    -e DRUPAL_DATABASE_DRIVER=mysql \
-    -e AUTO_INSTALL_PORTAL=true \
-    -v public-files:/app/code/web/sites/default/files \
-    -v private-files:/app/code/web/sites/default/private \
-	ghcr.io/apigee/docker-apigee-drupal-kickstart:latest
+docker compose --file=docker-compose-demo.yml up
